552973bdad78182135651c949eec0009d94d7d9f
Vincent <pinxue.career@gmail.com>
2020-11-05 01:40:06 +0800

    Create main function

--- ch4/treesort.go
+++ ch4/treesort.go
@@ -1,6 +1,10 @@
 // 以struct實現樹狀結構
 
-package treesort
+package main
+
+import(
+  "fmt"
+)
 
 type tree struct {
   value       int
@@ -13,15 +17,15 @@ func Sort(values []int) {
   for _, v := range values{
     root = add(root, v)
   }
-  appendValues(values[:0], root)
+  appendValues(values[:0], root)    // value[:0] 實際上就是整個slice
 }
 
 //依序插入元素t
 //回傳結果slice
 func appendValues(values []int, t *tree) []int {
   if t != nil {
-    values = appendValues(values, t.left)
-    values = append(values, t.value)
+    values = appendValues(values, t.left)   //先取最左(最小)
+    values = append(values, t.value)        //append到slice
     values = appendValues(values, t.right)
   }
   return values
@@ -30,6 +34,7 @@ func appendValues(values []int, t *tree) []int {
 func add(t *tree, value int) *tree {
   if t == nil {
     // 等同回傳&tree{value: value}
+    // 遞迴會一直return回tree的根結點t
     t = new(tree)
     t.value = value
     return t
@@ -41,4 +46,13 @@ func add(t *tree, value int) *tree {
     t.right = add(t.right, value)
   }
   return t
+}
+
+func main() {
+  arr := [5]int{2,1,5,4,3}
+  slice := arr[:]
+  fmt.Println("Before sorted: ", slice)
+  Sort(slice)
+  fmt.Println("After sorted: ", slice)
+
 }
